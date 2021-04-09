b1e12bbf1f33cf0c21ae936082f1e7b1cefec9cb
George Svitenko <georg.svit@gmail.com>
2021-04-09 12:45:27 +0300

    Register LandingComponent and HomeComponent dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,22 @@ import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 // Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     HttpClientModule,
@@ -26,6 +32,8 @@ import { LoginComponent } from "./login/login.component";
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
     FormsModule
   ],
   providers: [],
