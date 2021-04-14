709c234d5c5a4f42e7997ee9ce332d3829e438a5
George Svitenko <georg.svit@gmail.com>
2021-04-14 21:11:03 +0300

    Add locker and data management endpoints urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,11 @@ export const environment = {
   production: false,
   baseUrl: BASE_URL,
   endpoints: {
-    login: BASE_URL + '/account/login'
+    login: BASE_URL + '/account/login',
+    locker: BASE_URL + '/locker',
+    blockLocker: BASE_URL + '/locker/block/{id}',
+    unblockLocker: BASE_URL + '/locker/unblock/{id}',
+    data: BASE_URL + '/data',
+    backup: BASE_URL + '/data/backup',
   }
 };
