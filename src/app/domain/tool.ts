c254abdcb961a835bcf92cc5a1a4f23a2d9c0d1f
George Svitenko <georg.svit@gmail.com>
2021-04-14 21:14:35 +0300

    Add tool domain class

---
+++ src/app/domain/tool.ts
@@ -0,0 +1,10 @@
+export class Tool {
+    public id: string;
+    public name: string;
+    public description: string;
+    public imgUrl: string;
+    public accessLevel: number;
+    public userId: string;
+    public serviceBookId: string;
+    public lockerId: string;
+}
